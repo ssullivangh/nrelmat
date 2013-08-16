b70b2b22559862ab8fee7418b020c57a6946c954
S Sullivan <stephen.sullivan@nrel.gov>
2013-08-16 07:45:45 -0600

    Add licensing info

--- nrelmat/fillDbVasp.py
+++ nrelmat/fillDbVasp.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# Copyright 2013 National Renewable Energy Laboratory, Golden CO, USA
+# This file is part of NREL MatDB.
+#
+# NREL MatDB is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# NREL MatDB is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NREL MatDB.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime, hashlib, json, os, re, sys
 import numpy as np
