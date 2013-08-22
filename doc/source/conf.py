dcf8790159a38829a7ad6380bcba103feb21d164
S Sullivan <stephen.sullivan@nrel.gov>
2013-08-22 07:56:23 -0600

    lots of new documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,7 @@ print '\nconf.py sys.path: %s\n' % (sys.path,)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
