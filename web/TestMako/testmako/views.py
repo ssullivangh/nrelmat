00cc943e1532f3bc79b4bcf067ff764963aa897b
S Sullivan <stephen.sullivan@nrel.gov>
2013-10-11 13:55:30 -0600

    views: import gzip since tarfile does dyn import

--- web/TestMako/testmako/views.py
+++ web/TestMako/testmako/views.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NREL MatDB.  If not, see <http://www.gnu.org/licenses/>.
 
+import gzip   # preload gzip since tarfile loads it dynamically
 import json, os, re, StringIO, sys, tarfile, traceback
 import psycopg2
 from pyramid.view import view_config
